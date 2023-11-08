8da0e3db82d9076a26d9c2c4fef9da7a49e252cb
Charles Hamilton <52973156+chamilton-ccn@users.noreply.github.com>
2023-11-07 20:38:14 -0500

    Ok, *actually* fixing `Unhandled Exception: Newtonsoft.Json.JsonSerializationException: Error getting value from 'Name' on 'HubSpot.NET.Api.Associations.Dto.AssociationTypeHubSpotModel'. ---> System.ArgumentNullException: Value cannot be null.`

--- HubSpot.NET/Api/Associations/Dto/AssociationTypeHubSpotModel.cs
+++ HubSpot.NET/Api/Associations/Dto/AssociationTypeHubSpotModel.cs
@@ -63,10 +63,11 @@ namespace HubSpot.NET.Api.Associations.Dto
         /// If this association type is defined in the AssociationType enum, then it is a HubSpot defined type and it
         /// won't have a name so there's no point in setting a value here.
         /// </summary>
-        [DataMember(Name = "name", EmitDefaultValue = false)]
+        [DataMember(Name = "name")]
         public string Name => Enum.IsDefined(typeof(AssociationType), AssociationTypeId)
             ? null
             : NonAlphanumeric.Replace($"{Label}_{FromObjectType}_{ToObjectType}".ToLower(), "_");
+        public bool ShouldSerializeName() => Name != null;
 
         private static Regex NonAlphanumeric => new Regex(@"[^a-zA-Z0-9]|\s+", RegexOptions.Compiled);
 
